5e0507458f9eeb06a4c95559614d83e85fe669df
tianyawy <tianya_wy@hotmail.com>
2017-02-05 21:24:40 +0800

    [victoryw] #n/a first hello world/props

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,36 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Image, Text, View } from 'react-native'
 
-export default class ReactNativeDemo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+
+class Greeting extends Component {
+
+    render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+
+        return (
+           <View>
+               <Text> Hello {this.props.name}! </Text>
+               <Image source={pic} style={{width: 193, height: 110}} />
+           </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class LotsOfGreetings extends Component {
+    render() {
+        return (
+            <View style={{alignItems: 'center'}}>
+                <Greeting name='Rexxar' />
+                <Greeting name='Jaina' />
+                <Greeting name='Valeera' />
+            </View>
+        );
+    }
+}
 
-AppRegistry.registerComponent('ReactNativeDemo', () => ReactNativeDemo);
+AppRegistry.registerComponent('ReactNativeDemo', () => LotsOfGreetings);
